5f024282bc03d94ba73c30d859acdc244da33326
Wimenxior Ramperle <wimenxiorramperle@gmail.com>
2025-12-31 17:24:13 +0800

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export { RouteMap } from "./components/RouteMap";
-export type { RouteMapProps } from "./components/RouteMap";
+export { RouteMap, type RouteMapProps } from "./components/RouteMap";
 export { useMapHeader } from "./hooks/useMapHeader";
 export { theme } from "./theme";
-export type { Theme } from "./theme";
+export type { PartialTheme, Theme, ThemeChart, ThemeColors, ThemeDots, ThemeMap, ThemeMarker, ThemeMarkerShape, ThemeShadows, ThemeTooltip } from "./theme";
 export { ThemeProvider, useTheme } from "./theme-provider";
-export type { RouteConfig } from "./types";
+export type { RouteConfig, SurfaceType } from "./types";
+
