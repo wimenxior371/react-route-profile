72cbac2d86a8fb78f5129b554718b556dc59b0e0
Wimenxior Ramperle <wimenxiorramperle@gmail.com>
2025-12-31 17:54:18 +0800

    fix SurfaceType export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
+
 export { RouteMap, type RouteMapProps } from "./components/RouteMap";
 export { useMapHeader } from "./hooks/useMapHeader";
 export { theme } from "./theme";
 export type { PartialTheme, Theme, ThemeChart, ThemeColors, ThemeDots, ThemeMap, ThemeMarker, ThemeMarkerShape, ThemeShadows, ThemeTooltip } from "./theme";
 export { ThemeProvider, useTheme } from "./theme-provider";
-export type { RouteConfig, SurfaceType } from "./types";
-
+export { SurfaceType, type RouteConfig } from "./types";
